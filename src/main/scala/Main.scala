577bc33b12777f155b22c82379e86ec207948729
Long Cao <longcao@gmail.com>
2016-03-05 20:55:36 -0500

    add a bit of comment documentation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -56,6 +56,9 @@ object Main extends App {
       FullyEnrichedTaxiRide(trwd, pricePerDistance)
     }
 
+  /**
+   * Sink that indiscriminately tallies up and prints the count of elements it has seen.
+   */
   def sumElementsSink[T] = Sink.fold[Int, T](0) { (sum, _) =>
     val newSum = sum + 1
     if (newSum % 5000 == 0) {
@@ -64,6 +67,16 @@ object Main extends App {
     newSum
   }
 
+  /**
+   * Function that wraps over some less-than-ideal Promise-using code.
+   *
+   * Since the Subscriber to our elements isn't able to give us a handle to a Future
+   * that may eventually complete, create one by passing a callback to the elastic4s subscriber
+   * that will be called when the Subscriber completes, which completes our Promise.
+   *
+   * This function weaves together the akka-stream pipeline so that we stream elements
+   * from the Source through a pair of processing Flows, which terminate at a receiving Sink.
+   */
   def bulkInsertToElasticsearch: Future[Unit] = {
     val p = Promise[Unit]()
 
@@ -74,16 +87,18 @@ object Main extends App {
         errorFn = { (t: Throwable) => p.failure(t); ()})(FullyEnrichedTaxiRide.builder("nyc-taxi-rides"), system)
     }
 
-    taxiRidesSource // sequence the streaming, whose sink will complete the promise
-      .via(addFakeDescriptionFlow(fakeApiCall))
+    taxiRidesSource                             // sequence the streaming, whose sink will complete the promise
+      .via(addFakeDescriptionFlow(fakeApiCall)) // Source elements directed through this Flow
       .via(addPricePerDistanceRatioFlow)
-      .alsoTo(sumElementsSink)
-      .runWith(esSink)
+      .alsoTo(sumElementsSink)                  // fork the stream flow to an alternate end that counts
+      .runWith(esSink)                          // starts the streaming by specifying a target Sink
 
     p.future
   }
 
+  // wait for the underlying Promise to be complete (the indexing to be complete)
   Await.result(bulkInsertToElasticsearch, Duration.Inf)
 
+  // shut down the Akka ActorSystem nicely so we can exit
   Await.result(system.terminate, Duration.Inf)
 }
